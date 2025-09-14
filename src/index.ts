d19c5f5880eb10e729e4cecfb3b21d93392cbe0e
StageDog <aksanajw845@gmail.com>
2025-09-14 22:53:06 +0800

    提示 APP_READY

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { createApp } from 'vue';
 const app = createApp(Panel).use(createPinia());
 function initPanel() {
   const $app = $('<div id="tavern_helper_new">').appendTo('#extensions_settings');
+  // TODO: 一些组件应该在 APP_READY 后才加载
   app.mount($app[0]);
 }
 
