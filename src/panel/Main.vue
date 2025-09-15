e5fd47beb76dbf107d915052e755c647ab975e1c
StageDog <aksanajw845@gmail.com>
2025-09-15 16:54:18 +0800

    包装 Main

--- src/panel/Main.vue
+++ src/panel/Main.vue
@@ -1,23 +1,25 @@
 <template>
-  <Item>
-    <template #title>启用扩展</template>
-    <template #description>扩展总开关</template>
-    <template #content>
-      <Toggle id="tavern_helper_settings_enabled" v-model="enabled" />
-    </template>
-  </Item>
-  <Divider />
-  <Item type="plain">
-    <template #title>版本更新</template>
-    <template #description>查看最新特性，检查并更新扩展</template>
-    <template #content>
-      <Button :on-click="TODO">更新</Button>
-    </template>
-  </Item>
-  <Divider type="major"><i class="fa-solid fa-tools margin-r5"></i>开发工具</Divider>
-  <Reference />
-  <Divider type="major"><i class="fa-solid fa-shapes margin-r5"></i>扩展信息</Divider>
-  <Info />
+  <div>
+    <Item>
+      <template #title>启用扩展</template>
+      <template #description>扩展总开关</template>
+      <template #content>
+        <Toggle id="tavern_helper_settings_enabled" v-model="enabled" />
+      </template>
+    </Item>
+    <Divider />
+    <Item type="plain">
+      <template #title>版本更新</template>
+      <template #description>查看最新特性，检查并更新扩展</template>
+      <template #content>
+        <Button :on-click="TODO">更新</Button>
+      </template>
+    </Item>
+    <Divider type="major"><i class="fa-solid fa-tools margin-r5"></i>开发工具</Divider>
+    <Reference />
+    <Divider type="major"><i class="fa-solid fa-shapes margin-r5"></i>扩展信息</Divider>
+    <Info />
+  </div>
 </template>
 
 <script setup lang="ts">
