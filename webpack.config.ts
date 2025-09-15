7c7e06109778d53ee22714d07fb4b8239dd6cc06
StageDog <aksanajw845@gmail.com>
2025-09-15 16:32:48 +0800

    本地打包 production 时开启 devtools

--- webpack.config.ts
+++ webpack.config.ts
@@ -37,7 +37,7 @@ const config = (_env: any, argv: any): webpack.Configuration => {
       new VueLoaderPlugin(),
       new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: false,
-        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_DEVTOOLS__: process.env.CI !== 'true',
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
       }),
       new MiniCssExtractPlugin(),
