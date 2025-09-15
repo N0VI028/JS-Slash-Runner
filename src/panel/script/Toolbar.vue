84504f9e4798e6194fd5bc98b28f602709c200e0
StageDog <aksanajw845@gmail.com>
2025-09-15 17:48:50 +0800

    为脚本库添加搜索框

--- src/panel/script/Toolbar.vue
+++ src/panel/script/Toolbar.vue
@@ -7,9 +7,18 @@
       <Button type="tavern" :on-click="TODO"><i class="fa-solid fa-archive" /><small>内置库</small></Button>
     </div>
   </div>
+
+  <div class="width100p flex-container alignItemsCenter">
+    <SearchBar v-model="search_input" placeholder="搜索（支持普通和正则）" clearable />
+  </div>
 </template>
 
 <script setup lang="ts">
 import Button from '@/panel/component/Button.vue';
+import SearchBar from '@/panel/component/SearchBar.vue';
 import { TODO } from '@/todo';
+import { ref, watch } from 'vue';
+
+const search_input = ref('');
+watch(search_input, TODO);
 </script>
