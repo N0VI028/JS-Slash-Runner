c549caf5334287d0de75d78eb9a6e222b724f30e
StageDog <aksanajw845@gmail.com>
2025-09-26 15:43:47 +0800

    Revert "feat: 允许 useEventSourceOn 绑定 boolean"
    This reverts commit fe0592013a8a7742a79257b298a8f0af2b271e5b.

--- src/panel/composable/use_event_source_on.ts
+++ src/panel/composable/use_event_source_on.ts
@@ -3,29 +3,22 @@ import { eventSource } from '@sillytavern/script';
 export function useEventSourceOn(
   event: MaybeRefOrGetter<string>,
   callback: MaybeRefOrGetter<(...args: any[]) => void>,
-  should_enable: MaybeRefOrGetter<boolean> = true,
 ): () => void {
   const stop_watch = watch(
-    () => [toValue(event), unref(callback), toValue(should_enable)] as const,
-    ([new_event, new_callback, new_should_enable], old) => {
+    () => [toValue(event), unref(callback)] as const,
+    ([new_event, new_callback], old) => {
       if (old) {
-        const [old_event, old_callback, old_should_enable] = old;
-        if (old_should_enable) {
-          eventSource.removeListener(old_event, old_callback);
-        }
+        const [old_event, old_callback] = old;
+        eventSource.removeListener(old_event, old_callback);
       }
-      if (new_should_enable) {
-        eventSource.on(new_event, new_callback);
-      }
+      eventSource.on(new_event, new_callback);
     },
     { immediate: true },
   );
 
   const stop = () => {
     stop_watch();
-    if (toValue(should_enable)) {
-      eventSource.removeListener(toValue(event), unref(callback));
-    }
+    eventSource.removeListener(toValue(event), unref(callback));
   };
 
   tryOnScopeDispose(stop);
