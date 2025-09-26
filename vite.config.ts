afd812e96b66fcd81fdcba5bd7e0fbd2f65147f6
StageDog <aksanajw845@gmail.com>
2025-09-26 15:11:43 +0800

    chore: inline sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -87,7 +87,7 @@ export default defineConfig(({ mode }) => ({
     outDir: 'dist',
     emptyOutDir: true,
 
-    sourcemap: true,
+    sourcemap: mode === 'production' ? true : 'inline',
 
     minify: mode === 'production' ? 'terser' : false,
     terserOptions:
