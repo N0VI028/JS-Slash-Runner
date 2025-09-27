3c068a6d86524268138f6d3d9eb67fd891ce73ee
StageDog <aksanajw845@gmail.com>
2025-09-27 13:10:08 +0800

    fix: 消息渲染无需 async

--- src/panel/render/use_render_iframe.ts
+++ src/panel/render/use_render_iframe.ts
@@ -2,34 +2,32 @@ import Iframe from '@/panel/render/Iframe.vue';
 import { reloadChatWithoutEvents } from '@/util/reload_chat';
 import { chat, event_types, eventSource } from '@sillytavern/script';
 
-async function renderCodeBlock($pre: JQuery<HTMLPreElement>, id: string, with_loading: boolean, use_blob_url: boolean) {
+function renderCodeBlock($pre: JQuery<HTMLPreElement>, id: string, with_loading: boolean, use_blob_url: boolean) {
   createApp(Iframe, { id, content: $pre.text(), withLoading: with_loading, useBlobUrl: use_blob_url }).mount($pre[0]);
 }
 
-async function renderCodeBlockForMessage($mes: JQuery<HTMLElement>, with_loading: boolean, use_blob_url: boolean) {
-  await Promise.all(
-    $mes.toArray().flatMap(div => {
-      const message_id = Number($(div).attr('mesid'));
-      return $(div)
-        .find('pre')
-        .filter((_index, pre) => $(pre).text().includes('<body'))
-        .map((index, pre) => {
-          return renderCodeBlock($(pre), `message-iframe-${message_id}-${index}`, with_loading, use_blob_url);
-        });
-    }),
-  );
+function renderCodeBlockForMessage($mes: JQuery<HTMLElement>, with_loading: boolean, use_blob_url: boolean) {
+  $mes.each((_index, div) => {
+    const message_id = Number($(div).attr('mesid'));
+    $(div)
+      .find('pre')
+      .filter((_index, pre) => $(pre).text().includes('<body'))
+      .each((index, pre) => {
+        renderCodeBlock($(pre), `message-iframe-${message_id}-${index}`, with_loading, use_blob_url);
+      });
+  });
 }
 
-async function renderCodeBlockForMessageId(message_id: number, with_loading: boolean, use_blob_url: boolean) {
-  await renderCodeBlockForMessage($(`.mes[mesid=${message_id}]`), with_loading, use_blob_url);
+function renderCodeBlockForMessageId(message_id: number, with_loading: boolean, use_blob_url: boolean) {
+  renderCodeBlockForMessage($(`.mes[mesid=${message_id}]`), with_loading, use_blob_url);
 }
 
-async function renderCodeBlockForAll(with_loading: boolean, use_blob_url: boolean, depth: number) {
+function renderCodeBlockForAll(with_loading: boolean, use_blob_url: boolean, depth: number) {
   let $mes = $('.mes');
   if (depth !== 0) {
     $mes = $mes.filter((_index, div) => chat.length - Number($(div).attr('mesid')) <= depth);
   }
-  await renderCodeBlockForMessage($mes, with_loading, use_blob_url);
+  renderCodeBlockForMessage($mes, with_loading, use_blob_url);
 }
 
 function derenderCodeBlockForAll() {
@@ -44,9 +42,9 @@ export function useRenderIframe(
 ) {
   watch(
     enabled,
-    async (value, old_value) => {
+    (value, old_value) => {
       if (value) {
-        await renderCodeBlockForAll(with_loading.value, use_blob_url.value, depth.value);
+        renderCodeBlockForAll(with_loading.value, use_blob_url.value, depth.value);
         return;
       }
       if (!value && old_value) {
@@ -63,15 +61,15 @@ export function useRenderIframe(
     event_types.MESSAGE_SWIPED,
   ];
   render_message_id_events.forEach(event => {
-    eventSource.on(event, async (message_id: string | number) => {
+    eventSource.on(event, (message_id: string | number) => {
       if (enabled.value) {
-        await renderCodeBlockForMessageId(Number(message_id), with_loading.value, use_blob_url.value);
+        renderCodeBlockForMessageId(Number(message_id), with_loading.value, use_blob_url.value);
       }
     });
   });
-  eventSource.on(event_types.MESSAGE_DELETED, async () => {
+  eventSource.on(event_types.MESSAGE_DELETED, () => {
     if (enabled.value) {
-      await renderCodeBlockForAll(with_loading.value, use_blob_url.value, depth.value);
+      renderCodeBlockForAll(with_loading.value, use_blob_url.value, depth.value);
     }
   });
 }
