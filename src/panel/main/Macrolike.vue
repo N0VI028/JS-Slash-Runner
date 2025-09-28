e7c5755b508f22b65025bd5fbe9ae3f7a17e25ca
StageDog <aksanajw845@gmail.com>
2025-09-28 11:58:56 +0800

    typo

--- src/panel/main/Macrolike.vue
+++ src/panel/main/Macrolike.vue
@@ -2,7 +2,7 @@
   <Item type="box">
     <template #title>{{ t`禁用酒馆助手宏` }}</template>
     <template #description>
-      {{ t`编写变量角色卡而非测试/游玩角色卡时，请打开此开关，避免 \{\{get_message_variable::变量\}\} 等宏被替换` }}
+      {{ t`编写变量角色卡而非测试/游玩角色卡时，打开此开关，避免 \{\{get_message_variable::变量\}\} 等宏被替换` }}
     </template>
     <template #content>
       <Toggle :id="`TH-macro-enabled`" v-model="disabled" />
