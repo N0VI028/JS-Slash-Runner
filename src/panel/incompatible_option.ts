b67a5a325593132061a4ebe214509b144f890b15
StageDog <aksanajw845@gmail.com>
2025-10-04 18:34:12 +0800

    强制勾选允许 {{char}} 和 {{user}}

--- src/panel/incompatible_option.ts
+++ src/panel/incompatible_option.ts
@@ -1,20 +1,32 @@
 import { saveSettingsDebounced } from '@sillytavern/script';
 import { power_user } from '@sillytavern/scripts/power-user';
 
-function toggleIfNotAllowed(setting: string, expected: boolean): boolean {
-  if (_.get(power_user, setting) === expected) {
+interface ExpectedOption {
+  setting: string;
+  expected: boolean;
+}
+
+function toggleIfNotAllowed(option: ExpectedOption): boolean {
+  if (_.get(power_user, option.setting) === option.expected) {
     return false;
   }
 
-  _.set(power_user, setting, expected);
-  $(`#${setting}`).prop('checked', expected);
+  _.set(power_user, option.setting, option.expected);
+  $(`#${option.setting}`).prop('checked', option.expected);
   return true;
 }
 
 export function disableIncompatibleOption() {
   if (
-    ['auto_fix_generated_markdown', 'trim_sentences', 'forbid_external_media', 'encode_tags']
-      .map(setting => toggleIfNotAllowed(setting, false))
+    [
+      { setting: 'auto_fix_generated_markdown', expected: false },
+      { setting: 'trim_sentences', expected: false },
+      { setting: 'forbid_external_media', expected: false },
+      { setting: 'encode_tags', expected: false },
+      { setting: 'allow_name1_display', expected: true },
+      { setting: 'allow_name2_display', expected: true },
+    ]
+      .map(option => toggleIfNotAllowed(option))
       .some(is_changed => !!is_changed)
   ) {
     saveSettingsDebounced();
