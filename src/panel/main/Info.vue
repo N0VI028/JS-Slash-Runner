bdfd124cfd39b2af15395e6d17511e123ff7efbe
N0VI028 <Hardware9939@proton.me>
2025-10-20 15:26:30 +0800

    更新discord链接

--- src/panel/main/Info.vue
+++ src/panel/main/Info.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative flex w-full justify-between gap-1 rounded-md bg-[var(--grey5020a)] p-1.5">
+  <div class="relative flex w-full justify-between gap-1 rounded-md bg-(--grey5020a) p-1.5">
     <!-- prettier-ignore-attribute -->
     <div class="flex flex-1 flex-col text-sm opacity-70">
       <div class="mb-1.5">
@@ -18,12 +18,25 @@
       <a href="https://github.com/N0VI028/JS-Slash-Runner" target="_blank">
         <i class="fa-brands fa-github"></i>
       </a>
-      <a
-        href="https://discord.com/channels/1291925535324110879/1363482767421341868/1363482767421341868"
-        target="_blank"
-      >
-        <i class="fa-brands fa-discord"></i>
-      </a>
+      <Tippy trigger="click" placement="top" :interactive="true">
+        <i class="fa-brands fa-discord cursor-pointer text-(--SmartThemeQuoteColor)"></i>
+        <template #content>
+          <!-- prettier-ignore-attribute -->
+          <div class="flex cursor-pointer flex-col gap-0.25 rounded-sm bg-(--SmartThemeBlurTintColor) p-0.5">
+            <a
+              href="https://discord.com/channels/1134557553011998840/1296494001406345318"
+              target="_blank"
+              ><span class="text-(--SmartThemeBodyColor)">{{ t`类脑` }}</span></a
+            >
+            <div class="h-[0.5px] w-full bg-(--SmartThemeEmColor)" />
+            <a
+              href="https://discord.com/channels/1291925535324110879/1374297592854216774"
+              target="_blank"
+              ><span class="text-(--SmartThemeBodyColor)">{{ t`旅程` }}</span></a
+            >
+          </div>
+        </template>
+      </Tippy>
     </div>
     <!-- prettier-ignore-attribute -->
     <div
@@ -33,3 +46,7 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Tippy } from 'vue-tippy';
+</script>
