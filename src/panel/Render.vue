e3de4468f17f6e931db1cc38aa1b7fd6ed935f85
StageDog <aksanajw845@gmail.com>
2025-10-21 21:21:40 +0800

    style: 代码块显示时不使用 inline-block

--- src/panel/Render.vue
+++ src/panel/Render.vue
@@ -88,7 +88,7 @@ const runtimes = toRef(useMessageIframeRuntimesStore(), 'runtimes');
 </script>
 
 <style>
-.TH-render:has(.TH-collapse-code-block-button:not([style*='display: none;'])) {
+.TH-render:has(.TH-collapse-code-block-button:not([style*='display: none'])):has(pre[style*='display: none']) {
   display: inline-block;
 }
 </style>
