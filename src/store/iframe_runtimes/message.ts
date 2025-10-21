4c4a6c66bc3e70b306c1ce31504f07e9af65605e
StageDog <aksanajw845@gmail.com>
2025-10-21 16:05:24 +0800

    fix: iframe 变窄

--- src/store/iframe_runtimes/message.ts
+++ src/store/iframe_runtimes/message.ts
@@ -16,7 +16,7 @@ function renderCodeBlockForMessage($mes: JQuery<HTMLElement>): MessageIframeRunt
           const $possible_div = $pre.prev('div.TH-render');
           return $possible_div.length > 0
             ? $possible_div[0]
-            : $('<div class="TH-render inline-block">').insertBefore($pre)[0];
+            : $('<div class="TH-render">').insertBefore($pre)[0];
         });
       return [message_id, $element.toArray()] as const;
     })
