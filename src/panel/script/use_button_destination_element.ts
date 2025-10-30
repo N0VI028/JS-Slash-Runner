70b1c7cd205377525c734c570b9f04c7e91b7cf6
StageDog <aksanajw845@gmail.com>
2025-10-30 18:05:24 +0800

    refactor

--- src/panel/script/use_button_destination_element.ts
+++ src/panel/script/use_button_destination_element.ts
@@ -2,45 +2,43 @@ import { APP_READY_EVENTS } from '@/util/tavern';
 import { eventSource } from '@sillytavern/script';
 import { extension_settings } from '@sillytavern/scripts/extensions';
 
+function recalculateElement($send_form: JQuery<HTMLElement>): HTMLElement {
+  // $('#qr--bar') 要求 id 唯一, 因此此处不使用
+  let $possible_qr_bar = $send_form.find('div').filter(function () {
+    return $(this).attr('id') === 'qr--bar';
+  });
+  if ($possible_qr_bar.length > 1) {
+    $possible_qr_bar.filter('.TH--qr--bar').remove();
+    $possible_qr_bar = $send_form.find('div').filter(function () {
+      return $(this).attr('id') === 'qr--bar';
+    });
+  }
+  const $qr_bar =
+    $possible_qr_bar.length > 0
+      ? $possible_qr_bar.first()
+      : $('<div id="qr--bar" class="TH--qr--bar flex-container flexGap5">').prependTo($send_form);
+  if (_.get(extension_settings, 'quickReplyV2.isCombined')) {
+    const $possible_qr_buttons = $qr_bar.children('.qr--buttons');
+    const $qr_buttons =
+      $possible_qr_buttons.length > 0 ? $possible_qr_buttons.first() : $('<div class="qr--buttons">').appendTo($qr_bar);
+    return $qr_buttons[0];
+  }
+  return $qr_bar[0];
+}
+
 export function useButtonDestinationElement(): Readonly<Ref<HTMLElement | null>> {
   const element = shallowRef<HTMLElement | null>(null);
   const force_key = ref<symbol>(Symbol());
-
   const $send_form = $('#send_form');
+
+  watch(force_key, () => {
+    element.value = recalculateElement($send_form);
+  });
 
   APP_READY_EVENTS.forEach(event =>
     eventSource.once(event, () => {
       force_key.value = Symbol();
     }),
-  );
-  watch(
-    force_key,
-    () => {
-      let $possible_qr_bar = $send_form.find('div').filter(function () {
-        return $(this).attr('id') === 'qr--bar';
-      });
-      if ($possible_qr_bar.length > 1) {
-        $possible_qr_bar.filter('.TH--qr--bar').remove();
-        $possible_qr_bar = $send_form.find('div').filter(function () {
-          return $(this).attr('id') === 'qr--bar';
-        });
-      }
-      const $qr_bar =
-        $possible_qr_bar.length > 0
-          ? $possible_qr_bar.first()
-          : $('<div id="qr--bar" class="TH--qr--bar flex-container flexGap5">').prependTo($send_form);
-      if (_.get(extension_settings, 'quickReplyV2.isCombined')) {
-        const $possible_qr_buttons = $qr_bar.children('.qr--buttons');
-        const $qr_buttons =
-          $possible_qr_buttons.length > 0
-            ? $possible_qr_buttons.first()
-            : $('<div class="qr--buttons">').appendTo($qr_bar);
-        element.value = $qr_buttons[0];
-        return;
-      }
-      element.value = $qr_bar[0];
-    },
-    { immediate: true },
   );
 
   $(document)
