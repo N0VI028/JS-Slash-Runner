60f8941aa4477230c3c8dcc1e587d9cbb9f302b4
StageDog <aksanajw845@gmail.com>
2025-11-10 23:58:40 +0800

    fix: 避免移除第三方 iframe 界面

--- src/panel/render/macro_like.ts
+++ src/panel/render/macro_like.ts
@@ -57,7 +57,7 @@ function demacroOnRender($mes: JQuery<HTMLDivElement>) {
   };
 
   // 因未知原因, 一些设备上在初次进入角色卡时会 '渲染前端界面-替换助手宏-渲染前端界面', 因此需要移除额外渲染的 iframe
-  $mes_text.find('iframe').remove();
+  $mes_text.find('.TH-render > iframe').remove();
 
   $mes_text.html((_index, html) => replace_html(html));
   $mes_text
