852825e22f365fe54cae459acdef4c9ffcdc919c
StageDog <aksanajw845@gmail.com>
2025-12-14 00:25:20 +0800

    fix: MESSAGE_SWIPE_DELETED 与提示词模板的兼容性

--- src/swipe.ts
+++ src/swipe.ts
@@ -7,7 +7,18 @@ export function registerSwipeEvent() {
     eventSource.on(
       event_types.MESSAGE_SWIPE_DELETED,
       ({ message_id, swipe_id }: { message_id: number; swipe_id: number }) => {
-        chat[message_id]?.variables?.splice(swipe_id, 1);
+        const variables = chat[message_id];
+        if (_.isArray(variables)) {
+          variables.splice(swipe_id, 1);
+          return;
+        }
+        if (_.isPlainObject(variables)) {
+          _.unset(variables, swipe_id);
+          _.mapKeys(variables, (_, key) => {
+            const current_id = Number(key);
+            return current_id < swipe_id ? String(current_id) : String(current_id - 1);
+          });
+        }
       },
     );
   }
