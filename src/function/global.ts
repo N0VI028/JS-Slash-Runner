a74110c4f6bba955e2bb7647495d8e579cad7d25
StageDog <aksanajw845@gmail.com>
2025-12-30 16:45:31 +0800

    feat: 优化 waitGlobalInitialized 在 mvu 上的表现

--- src/function/global.ts
+++ src/function/global.ts
@@ -1,6 +1,8 @@
 import { _eventEmit, _eventOnce } from '@/function/event';
 import { eventSource } from '@sillytavern/script';
+import { waitUntil } from 'async-wait-until';
 import { LiteralUnion } from 'type-fest';
+import { get_variables_without_clone } from './variables';
 
 export function initializeGlobal(global: LiteralUnion<'Mvu', string>, value: any): void {
   _.set(window, global, value);
@@ -29,14 +31,20 @@ export async function _waitGlobalInitialized(this: Window, global: LiteralUnion<
       get: () => _.get(window, global),
       configurable: true,
     });
+    if (global === 'Mvu') {
+      await waitUntil(() => _.has(get_variables_without_clone({ type: 'message', message_id: 0 }), 'stat_data'));
+    }
     return;
   }
   return new Promise(resolve => {
-    _eventOnce.call(this, `global_${global}_initialized`, () => {
+    _eventOnce.call(this, `global_${global}_initialized`, async () => {
       Object.defineProperty(this, global, {
         get: () => _.get(window, global),
         configurable: true,
       });
+      if (global === 'Mvu') {
+        await waitUntil(() => _.has(get_variables_without_clone({ type: 'message', message_id: 0 }), 'stat_data'));
+      }
       resolve();
     });
   });
