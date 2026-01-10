a678a404d95ca19ac014137c813df59c571e3577
StageDog <aksanajw845@gmail.com>
2026-01-11 00:35:06 +0800

    fix: 同行 {{format_message_variable::变量}} 的显示 [release]

--- src/function/macro_like.ts
+++ src/function/macro_like.ts
@@ -13,6 +13,50 @@ export interface MacroLikeContext {
   role?: 'user' | 'assistant' | 'system';
 }
 
+function getVariableOption(context: MacroLikeContext, type: 'message' | 'chat' | 'character' | 'preset' | 'global') {
+  return type !== 'message'
+    ? { type }
+    : {
+        type,
+        message_id:
+          context.message_id ?? chat.findLastIndex(message => _.isObject(message.variables?.[message.swipe_id ?? 0])),
+      };
+}
+function getWithout$(variables: Record<string, any>, path: string) {
+  return omitDeepBy(_.get(variables, _.unescape(path), null), (_, key) => key.startsWith('$'));
+}
+
+const format_variable_regex = /^(.*)\{\{format_(message|chat|character|preset|global)_variable::(.*?)\}\}/im;
+function applyFormatVariable(
+  context: MacroLikeContext,
+  _substring: string,
+  prefix: string,
+  type: 'message' | 'chat' | 'character' | 'preset' | 'global',
+  path: string,
+) {
+  const match = prefix.match(format_variable_regex);
+  if (match) {
+    prefix =
+      applyFormatVariable(
+        context,
+        '',
+        match[1],
+        match[2] as 'message' | 'chat' | 'character' | 'preset' | 'global',
+        match[3],
+      ) + prefix.slice(match[0].length);
+  }
+
+  const variables = get_variables_without_clone(getVariableOption(context, type));
+  const value = getWithout$(variables, path);
+  return (
+    prefix +
+    (typeof value === 'string' ? value : YAML.stringify(value, { blockQuote: 'literal' }).trimEnd()).replaceAll(
+      '\n',
+      '\n' + ' '.repeat(prefix.length),
+    )
+  );
+}
+
 export const macros: MacroLike[] = [
   {
     regex: /\{\{get_(message|chat|character|preset|global)_variable::(.*?)\}\}/gi,
@@ -22,48 +66,14 @@ export const macros: MacroLike[] = [
       type: 'message' | 'chat' | 'character' | 'preset' | 'global',
       path: string,
     ) => {
-      const variables = get_variables_without_clone(
-        type !== 'message'
-          ? { type }
-          : {
-              type,
-              message_id:
-                context.message_id ??
-                chat.findLastIndex(message => _.isObject(message.variables?.[message.swipe_id ?? 0])),
-            },
-      );
-      const value = omitDeepBy(_.get(variables, _.unescape(path), null), (_, key) => key.startsWith('$'));
+      const variables = get_variables_without_clone(getVariableOption(context, type));
+      const value = getWithout$(variables, path);
       return typeof value === 'string' ? value : JSON.stringify(value);
     },
   },
   {
     regex: /^(.*)\{\{format_(message|chat|character|preset|global)_variable::(.*?)\}\}/gim,
-    replace: (
-      context: MacroLikeContext,
-      _substring: string,
-      prefix: string,
-      type: 'message' | 'chat' | 'character' | 'preset' | 'global',
-      path: string,
-    ) => {
-      const variables = get_variables_without_clone(
-        type !== 'message'
-          ? { type }
-          : {
-              type,
-              message_id:
-                context.message_id ??
-                chat.findLastIndex(message => _.isObject(message.variables?.[message.swipe_id ?? 0])),
-            },
-      );
-      const value = omitDeepBy(_.get(variables, _.unescape(path), null), (_, key) => key.startsWith('$'));
-      return (
-        prefix +
-        (typeof value === 'string' ? value : YAML.stringify(value, { blockQuote: 'literal' }).trimEnd()).replaceAll(
-          '\n',
-          '\n' + ' '.repeat(prefix.length),
-        )
-      );
-    },
+    replace: applyFormatVariable,
   },
 ];
 
