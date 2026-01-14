3a36de4352032425ebd5684dcca4cdbf829409bf
StageDog <aksanajw845@gmail.com>
2026-01-14 13:58:11 +0800

    fix: 调整iframe第三方库顺序

--- src/panel/script/iframe.ts
+++ src/panel/script/iframe.ts
@@ -5,9 +5,9 @@ import third_party from '@/iframe/third_party_script.html?raw';
 export function createSrcContent(content: string, use_blob_url: boolean) {
   return `<html>
 <head>
-<script src="${parent_jquery_url}"></script>
-${third_party}
 ${use_blob_url ? `<base href="${window.location.origin}"/>` : ''}
+${third_party}
+<script src="${parent_jquery_url}"></script>
 <script src="${predefine_url}"></script>
 <script src="https://testingcf.jsdelivr.net/gh/N0VI028/JS-Slash-Runner/src/iframe/node_modules/log.js"></script>
 </head>
