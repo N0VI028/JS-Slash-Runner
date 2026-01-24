2b0a91468696eb07ae3d48bd246f808dc059884e
StageDog <aksanajw845@gmail.com>
2026-01-24 14:35:45 +0800

    feat: 为预设定义一些 extensions 类型

--- src/function/character.ts
+++ src/function/character.ts
@@ -1,11 +1,26 @@
-interface Character {
+import { TavernRegex } from '@/function/tavern_regex';
+
+// TODO: 角色卡头像
+type Character = {
   name: string;
-  first_messages: string[];
-  description: string;
-
-  extensions: Record<string, any>;
-
   version: string;
   creator: string;
   creator_notes: string;
-}
+
+  description: string;
+  first_messages: string[];
+
+  worldbook: {
+    primary: string | null;
+    additional: string[];
+  };
+
+  extensions: {
+    regex_scripts?: TavernRegex[];
+    tavern_helper?: {
+      scripts: Record<string, any>[];
+      variales: Record<string, any>;
+    };
+    [other: string]: any;
+  };
+};
