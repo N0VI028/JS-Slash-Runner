b9a142748cd2aec8ef73ab51a96dbb42afc36fd0
StageDog <aksanajw845@gmail.com>
2026-01-29 16:03:22 +0800

    fix: 删除消息时的逻辑

--- src/panel/render/use_better_chat_truncation.ts
+++ src/panel/render/use_better_chat_truncation.ts
@@ -1,5 +1,25 @@
-import { event_types, eventSource } from '@sillytavern/script';
+import { addOneMessage, chat, event_types, eventSource, scrollChatToBottom } from '@sillytavern/script';
 import { power_user } from '@sillytavern/scripts/power-user';
+
+export async function auditChatMessages() {
+  if (power_user.chat_truncation > 0) {
+    const mesid = $('#chat > .mes').first().attr('mesid');
+
+    const last_showed = mesid ? Number(mesid) : undefined;
+    if (last_showed === undefined || chat.length - power_user.chat_truncation < last_showed) {
+      const should_show = _.range(Math.max(0, chat.length - power_user.chat_truncation), last_showed ?? chat.length);
+      for (const message_id of should_show) {
+        const message = chat[message_id];
+        addOneMessage(message, { insertBefore: last_showed, forceId: message_id, scroll: false });
+        await eventSource.emit(
+          message.is_user ? event_types.USER_MESSAGE_RENDERED : event_types.CHARACTER_MESSAGE_RENDERED,
+          message_id,
+        );
+      }
+      scrollChatToBottom({ waitForFrame: true });
+    }
+  }
+}
 
 export function useBetterChatTruncation(enabled: Readonly<Ref<boolean>>) {
   $('#chat_truncation, #chat_truncation_counter').attr('step', 1);
@@ -7,16 +27,21 @@ export function useBetterChatTruncation(enabled: Readonly<Ref<boolean>>) {
     eventSource.on(event, () => {
       if (enabled.value && power_user.chat_truncation > 0) {
         const $mes = $('#chat > .mes');
-        if ($mes.length > power_user.chat_truncation && $('#show_more_messages').length === 0) {
-          $('#chat').append('<div id="show_more_messages">Show more messages</div>');
-        }
         $mes.slice(0, Math.max(0, $mes.length - power_user.chat_truncation)).remove();
+
+        const first_mesid = Number($mes.first().attr('mesid'));
+        const $show_more_messages = $('#show_more_messages');
+        if (first_mesid > 0 && $show_more_messages.length === 0) {
+          $('#chat').append('<div id="show_more_messages">Show more messages</div>');
+        } else if (first_mesid === 0 && $show_more_messages.length > 0) {
+          $show_more_messages.remove();
+        }
       }
     }),
   );
-  eventSource.on(event_types.MESSAGE_DELETED, () => {
-    if (enabled.value && Number($('#chat > .mes').first().attr('mesid')) > 0 && $('#show_more_messages').length === 0) {
-      $('#chat').append('<div id="show_more_messages">Show more messages</div>');
+  eventSource.on(event_types.MESSAGE_DELETED, async () => {
+    if (enabled.value) {
+      await auditChatMessages();
     }
   });
 }
